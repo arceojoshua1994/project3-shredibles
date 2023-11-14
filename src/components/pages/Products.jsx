57c1dfec816b13c36eccdde9dcc21c07a05cd1d5
arceojoshua94 <arceojoshua94@outlook.com>
2023-11-13 21:18:00 -0500

    update pages

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -12,12 +12,12 @@ export default function Products() {
           <div className="col-lg-3 col-md-6 mb-4 pb-2">
             <div className="product-item d-flex flex-column align-items-center text-center bg-light rounded py-5 px-3">
               <div className="bg-primary mt-n5 py-3" style={{ width: '80px' }}>
-                <h4 className="font-weight-bold text-white mb-0">$99</h4>
+                <h4 className="font-weight-bold text-white mb-0">$48</h4>
               </div>
               <div className="position-relative bg-primary rounded-circle mt-n3 mb-4 p-3" style={{ width: '150px', height: '150px' }}>
-                <img className="rounded-circle w-100 h-100" src="img/product-1.jpg" alt="Vanilla Ice Cream" style={{ objectFit: 'cover' }} />
+                <img className="rounded-circle w-100 h-100" src="./assets/images/products/Whey-Out Whey Protein.png" alt="Vanilla Ice Cream" style={{ objectFit: 'cover' }} />
               </div>
-              <h5 className="font-weight-bold mb-4">Vanilla Ice Cream</h5>
+              <h5 className="font-weight-bold mb-4">Whey-Out Protein</h5>
               <a href="#" className="btn btn-sm btn-secondary">Order Now</a>
             </div>
           </div>
@@ -25,12 +25,12 @@ export default function Products() {
           <div className="col-lg-3 col-md-6 mb-4 pb-2">
             <div className="product-item d-flex flex-column align-items-center text-center bg-light rounded py-5 px-3">
               <div className="bg-primary mt-n5 py-3" style={{ width: '80px' }}>
-                <h4 className="font-weight-bold text-white mb-0">$99</h4>
+                <h4 className="font-weight-bold text-white mb-0">$30</h4>
               </div>
               <div className="position-relative bg-primary rounded-circle mt-n3 mb-4 p-3" style={{ width: '150px', height: '150px' }}>
                 <img className="rounded-circle w-100 h-100" src="img/product-1.jpg" alt="Vanilla Ice Cream" style={{ objectFit: 'cover' }} />
               </div>
-              <h5 className="font-weight-bold mb-4">Vanilla Ice Cream</h5>
+              <h5 className="font-weight-bold mb-4">Blaze n Glazed Donuts</h5>
               <a href="#" className="btn btn-sm btn-secondary">Order Now</a>
             </div>
           </div>
@@ -38,12 +38,12 @@ export default function Products() {
           <div className="col-lg-3 col-md-6 mb-4 pb-2">
             <div className="product-item d-flex flex-column align-items-center text-center bg-light rounded py-5 px-3">
               <div className="bg-primary mt-n5 py-3" style={{ width: '80px' }}>
-                <h4 className="font-weight-bold text-white mb-0">$99</h4>
+                <h4 className="font-weight-bold text-white mb-0">$25</h4>
               </div>
               <div className="position-relative bg-primary rounded-circle mt-n3 mb-4 p-3" style={{ width: '150px', height: '150px' }}>
                 <img className="rounded-circle w-100 h-100" src="img/product-1.jpg" alt="Vanilla Ice Cream" style={{ objectFit: 'cover' }} />
               </div>
-              <h5 className="font-weight-bold mb-4">Vanilla Ice Cream</h5>
+              <h5 className="font-weight-bold mb-4">Puff Pass Cream Puffs</h5>
               <a href="#" className="btn btn-sm btn-secondary">Order Now</a>
             </div>
           </div>
@@ -51,12 +51,12 @@ export default function Products() {
           <div className="col-lg-3 col-md-6 mb-4 pb-2">
             <div className="product-item d-flex flex-column align-items-center text-center bg-light rounded py-5 px-3">
               <div className="bg-primary mt-n5 py-3" style={{ width: '80px' }}>
-                <h4 className="font-weight-bold text-white mb-0">$99</h4>
+                <h4 className="font-weight-bold text-white mb-0">$45</h4>
               </div>
               <div className="position-relative bg-primary rounded-circle mt-n3 mb-4 p-3" style={{ width: '150px', height: '150px' }}>
                 <img className="rounded-circle w-100 h-100" src="img/product-1.jpg" alt="Vanilla Ice Cream" style={{ objectFit: 'cover' }} />
               </div>
-              <h5 className="font-weight-bold mb-4">Vanilla Ice Cream</h5>
+              <h5 className="font-weight-bold mb-4">Canna-Bulk Cookies</h5>
               <a href="#" className="btn btn-sm btn-secondary">Order Now</a>
             </div>
           </div>
@@ -64,12 +64,12 @@ export default function Products() {
           <div className="col-lg-3 col-md-6 mb-4 pb-2">
             <div className="product-item d-flex flex-column align-items-center text-center bg-light rounded py-5 px-3">
               <div className="bg-primary mt-n5 py-3" style={{ width: '80px' }}>
-                <h4 className="font-weight-bold text-white mb-0">$99</h4>
+                <h4 className="font-weight-bold text-white mb-0">$60</h4>
               </div>
               <div className="position-relative bg-primary rounded-circle mt-n3 mb-4 p-3" style={{ width: '150px', height: '150px' }}>
                 <img className="rounded-circle w-100 h-100" src="img/product-1.jpg" alt="Vanilla Ice Cream" style={{ objectFit: 'cover' }} />
               </div>
-              <h5 className="font-weight-bold mb-4">Vanilla Ice Cream</h5>
+              <h5 className="font-weight-bold mb-4">LIFT3D Pre Workout</h5>
               <a href="#" className="btn btn-sm btn-secondary">Order Now</a>
             </div>
           </div>
@@ -77,12 +77,12 @@ export default function Products() {
           <div className="col-lg-3 col-md-6 mb-4 pb-2">
             <div className="product-item d-flex flex-column align-items-center text-center bg-light rounded py-5 px-3">
               <div className="bg-primary mt-n5 py-3" style={{ width: '80px' }}>
-                <h4 className="font-weight-bold text-white mb-0">$99</h4>
+                <h4 className="font-weight-bold text-white mb-0">$50</h4>
               </div>
               <div className="position-relative bg-primary rounded-circle mt-n3 mb-4 p-3" style={{ width: '150px', height: '150px' }}>
                 <img className="rounded-circle w-100 h-100" src="img/product-1.jpg" alt="Vanilla Ice Cream" style={{ objectFit: 'cover' }} />
               </div>
-              <h5 className="font-weight-bold mb-4">Vanilla Ice Cream</h5>
+              <h5 className="font-weight-bold mb-4">Gym-Rat Post-Workout Ganja Gummies</h5>
               <a href="#" className="btn btn-sm btn-secondary">Order Now</a>
             </div>
           </div>
--- src/components/pages/Reviews.jsx
+++ src/components/pages/Reviews.jsx
@@ -1,15 +1,40 @@
 export default function Reviews() {
-    return (
-      <div>
-        <h1>Reviews</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
+  return (
+    <div className="container-fluid py-5">
+      <div className="container py-5">
+          <div className="row justify-content-center">
+              <div className="col-lg-6">
+                  <h1 className="section-title position-relative text-center mb-5">Clients Say About Our Famous Ice Cream</h1>
+              </div>
+          </div>
+          <div className="row justify-content-center">
+              <div className="col-lg-8">
+                  <div className="owl-carousel testimonial-carousel">
+                      <div className="text-center">
+                          <i className="fa fa-3x fa-quote-left text-primary mb-4"></i>
+                          <h4 className="font-weight-light mb-4">Dolor eirmod diam stet kasd sed. Aliqu rebum est eos. Rebum elitr dolore et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</h4>
+                          <img className="img-fluid mx-auto mb-3" src="img/testimonial-1.jpg" alt="Client Testimonial" />
+                          <h5 className="font-weight-bold m-0">Client Name</h5>
+                          <span>Profession</span>
+                      </div>
+                      <div className="text-center">
+                          <i className="fa fa-3x fa-quote-left text-primary mb-4"></i>
+                          <h4 className="font-weight-light mb-4">Dolor eirmod diam stet kasd sed. Aliqu rebum est eos. Rebum elitr dolore et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</h4>
+                          <img className="img-fluid mx-auto mb-3" src="img/testimonial-2.jpg" alt="Client Testimonial" />
+                          <h5 className="font-weight-bold m-0">Client Name</h5>
+                          <span>Profession</span>
+                      </div>
+                      <div className="text-center">
+                          <i className="fa fa-3x fa-quote-left text-primary mb-4"></i>
+                          <h4 className="font-weight-light mb-4">Dolor eirmod diam stet kasd sed. Aliqu rebum est eos. Rebum elitr dolore et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</h4>
+                          <img className="img-fluid mx-auto mb-3" src="img/testimonial-3.jpg" alt="Client Testimonial" />
+                          <h5 className="font-weight-bold m-0">Client Name</h5>
+                          <span>Profession</span>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
-    );
-  }
+  </div>
+  );
+}
