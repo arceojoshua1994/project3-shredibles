0c98318b572af0367bf26ceff520daf6fb83c6ec
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-11-16 19:34:56 -0500

    Initial server commit

---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+const Tech = require('./Tech');
+const Matchup = require('./Matchup');
+const User = require('./User');
+const Product = require('./Product');
+const Category = require('./Category');
+const Order = require('./Order');
+
+module.exports = { Tech, Matchup, User, Product, Category, Order };
