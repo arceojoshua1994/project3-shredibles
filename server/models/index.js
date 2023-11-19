9a9b94a4200098ffca14b3152842221210095ac5
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-11-18 20:16:10 -0500

    Reverted to fix bugs and going to add server back

--- server/models/index.js
+++
@@ -1,8 +0,0 @@
-const Tech = require('./Tech');
-const Matchup = require('./Matchup');
-const User = require('./User');
-const Product = require('./Product');
-const Category = require('./Category');
-const Order = require('./Order');
-
-module.exports = { Tech, Matchup, User, Product, Category, Order };
