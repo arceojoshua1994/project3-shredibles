e3e74d8914a2cf3b930d77f1fe20cc2407336d53
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-11-19 23:11:47 -0500

    Updated Login.jsx with imports

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,9 +1,40 @@
-export default function Login() {
-    return (
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+function Login(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  return (
       <div className="container-fluid py-5">
         <div className="container py-5">
           <div className="row justify-content-center">
             <div className="col-lg-6">
+            <Link to="/signup">← Go to Signup</Link>
               <h1 className="section-title position-relative text-center mb-5">Sign Up / Log In</h1>
             </div>
           </div>
@@ -59,3 +90,5 @@ export default function Login() {
       </div>
     );
   }
+
+  export default Login;
