bbf2902ee7d039eb49b4fde2e70426fabcfa17cf
jusukarneh <135680474+jusukarneh@users.noreply.github.com>
2023-11-26 20:04:33 -0500

    crazy

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './scss/style.scss';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
