f79125e69a7245a73750687ac5af71058f7bc634
jusukarneh <135680474+jusukarneh@users.noreply.github.com>
2023-11-28 21:04:36 -0500

    sone

--- server/models/Tech.js
+++ server/models/Tech.js
@@ -1,13 +1,1 @@
-const { Schema, model } = require('mongoose');
 
-const Schema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-});
-
-const Tech = model('Tech', techSchema);
-
-module.exports = Tech;
