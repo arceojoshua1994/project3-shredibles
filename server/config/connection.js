fcc5ac8170a92cf054dbe16438a58a860439cccb
arceojoshua94 <arceojoshua94@outlook.com>
2023-12-01 17:50:41 -0500

    update connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:<Password123>@cluster0.ik8zwvi.mongodb.net/mern-shopping');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
 
 module.exports = mongoose.connection;
