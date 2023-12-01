b9ed4cf1fae2ef1ebc83a85b1b17b6c9d885a5ec
arceojoshua94 <arceojoshua94@outlook.com>
2023-12-01 17:12:05 -0500

    troubleshooting connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://arceojoshua:<#Bootycheeks69>@cluster0.ik8zwvi.mongodb.net/mern-shopping');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:<Password123>@cluster0.ik8zwvi.mongodb.net/mern-shopping');
 
 module.exports = mongoose.connection;
